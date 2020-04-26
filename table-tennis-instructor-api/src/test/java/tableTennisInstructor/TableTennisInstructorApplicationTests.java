582c462f65a92432bb776577d4fcae22dae5b98d
DusanBucan <dusanbzr@gmail.com>
2020-04-27 01:19:43 +0200

    a

--- table-tennis-instructor-api/src/test/java/tableTennisInstructor/TableTennisInstructorApplicationTests.java
+++ table-tennis-instructor-api/src/test/java/tableTennisInstructor/TableTennisInstructorApplicationTests.java
@@ -1,4 +1,4 @@
-package com.borisns.securitydemo;
+package tableTennisInstructor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecuritydemoApplicationTests {
+public class TableTennisInstructorApplicationTests {
 
 	@Test
 	public void contextLoads() {
